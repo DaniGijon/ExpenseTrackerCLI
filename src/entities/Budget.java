86a793dac9a50a63d33c05f7e5c61a69c9d5755d
DaniGijon <dani91gr@hotmail.com>
2025-10-15 12:28:35 +0200

    Budget feature unlocked

---
+++ src/entities/Budget.java
@@ -0,0 +1,127 @@
+package entities;
+
+public class Budget {
+	private double limitYear;
+	private double limitJanuary;
+	private double limitFebruary;
+	private double limitMarch;
+	private double limitApril;
+	private double limitMay;
+	private double limitJune;
+	private double limitJuly;
+	private double limitAugust;
+	private double limitSeptember;
+	private double limitOctober;
+	private double limitNovember;
+	private double limitDecember;
+	
+	public Budget() {
+		
+	}
+
+	public double getLimitYear() {
+		return limitYear;
+	}
+
+	public void setLimitYear(double limitYear) {
+		this.limitYear = limitYear;
+	}
+
+	public double getLimitJanuary() {
+		return limitJanuary;
+	}
+
+	public void setLimitJanuary(double limitJanuary) {
+		this.limitJanuary = limitJanuary;
+	}
+
+	public double getLimitFebruary() {
+		return limitFebruary;
+	}
+
+	public void setLimitFebruary(double limitFebruary) {
+		this.limitFebruary = limitFebruary;
+	}
+
+	public double getLimitMarch() {
+		return limitMarch;
+	}
+
+	public void setLimitMarch(double limitMarch) {
+		this.limitMarch = limitMarch;
+	}
+
+	public double getLimitApril() {
+		return limitApril;
+	}
+
+	public void setLimitApril(double limitApril) {
+		this.limitApril = limitApril;
+	}
+
+	public double getLimitMay() {
+		return limitMay;
+	}
+
+	public void setLimitMay(double limitMay) {
+		this.limitMay = limitMay;
+	}
+
+	public double getLimitJune() {
+		return limitJune;
+	}
+
+	public void setLimitJune(double limitJune) {
+		this.limitJune = limitJune;
+	}
+
+	public double getLimitJuly() {
+		return limitJuly;
+	}
+
+	public void setLimitJuly(double limitJuly) {
+		this.limitJuly = limitJuly;
+	}
+
+	public double getLimitAugust() {
+		return limitAugust;
+	}
+
+	public void setLimitAugust(double limitAugust) {
+		this.limitAugust = limitAugust;
+	}
+
+	public double getLimitSeptember() {
+		return limitSeptember;
+	}
+
+	public void setLimitSeptember(double limitSeptember) {
+		this.limitSeptember = limitSeptember;
+	}
+
+	public double getLimitOctober() {
+		return limitOctober;
+	}
+
+	public void setLimitOctober(double limitOctober) {
+		this.limitOctober = limitOctober;
+	}
+
+	public double getLimitNovember() {
+		return limitNovember;
+	}
+
+	public void setLimitNovember(double limitNovember) {
+		this.limitNovember = limitNovember;
+	}
+
+	public double getLimitDecember() {
+		return limitDecember;
+	}
+
+	public void setLimitDecember(double limitDecember) {
+		this.limitDecember = limitDecember;
+	}
+	
+	
+}
